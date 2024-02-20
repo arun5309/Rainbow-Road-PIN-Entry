7df19e1699f8308937b99456ca7e9aaefd4071b2
VanR20 <anrao3@wisc.edu>
2024-02-20 19:54:52 +0530

    Added rainbow road component which creates a specifiable number of permutations the user can choose from.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,39 @@
-<script>
+<script lang='ts'>
+	import KeyPad from "./KeyPad.svelte";
+	import RainbowRoad from "./RainbowRoad.svelte";
+	import {colors} from './utils'
+	let show_keypad_screen = false;
+	let show_rainbow_screen = true;
+
+	let value = '';
+	let rounds = 0;
+	let userEnteredPIN = '';
+
+	// from KeyPad
+	let keyPadColor:number = 4;
+
+	// from RainbowRoad
+	let numColors:number = 4;
+
+	function handleNumKeyPress(event:CustomEvent){
+		let num = event.detail.value;
+		if(value.length > 0){
+			console.log("no more input allowed")
+		} else {
+			value = value.concat(num)
+		}
+	}
+	function handleActionKeyPress(event:CustomEvent){
+		let key = event.detail.value;
+		if(key === 'backspace'){
+			value = value.substring(0,value.length-1);
+		} else if (key === 'enter'){
+			console.log("next round");
+			userEnteredPIN = userEnteredPIN.concat();
+		} else {
+			console.log("something other than 'backspace' or 'enter' was sent")
+		}
+	}
 
 </script>
 
@@ -8,13 +43,15 @@
 </svelte:head>
 
 <section>
-	<h1>
-		Welcome to your new<br />SvelteKit app
-	</h1>
+	{#if show_keypad_screen}
+	<h1>Rainbow Road PIN Entry</h1>
+	<input id="PIN_Entry" bind:value /><br>
+	<KeyPad {keyPadColor} on:numKey={handleNumKeyPress} on:actionKey={handleActionKeyPress}/>
+	{/if}
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+	{#if show_rainbow_screen}
+	<RainbowRoad rounds={4} numColors={numColors}/>
+	{/if}
 </section>
 
 <style>
@@ -28,5 +65,9 @@
 
 	h1 {
 		width: 100%;
+	}
+
+	#PIN_Entry {
+		min-width: 80vw;
 	}
 </style>
---
+++ src/routes/RainbowRoad.svelte
@@ -0,0 +1,82 @@
+<script lang='ts'>
+    import { colors, colorsNames, shuffle, getPermutationMatrix, random1toNOrder, gameHeader } from './utils'
+    import {createEventDispatcher } from 'svelte'
+    const dispatch = createEventDispatcher();
+
+    function pickRoad(c:String,v:Array<number>){
+        dispatch('pickRoad',{
+            color:c,
+            value:v
+        })
+    }
+
+    export let rounds:number;
+    export let numColors:number;
+
+    let colorsUsed = colorsNames.slice(0,numColors);
+    let pMatrix = getPermutationMatrix(numColors);
+    let colorPresentationOrder = random1toNOrder(numColors);
+
+</script>
+
+<table id="rainbowRoad">
+    <thead>
+        <!-- Define headers
+        1st and Last columns are for user reference
+        Middle Column Titles rendered to match the color order of the permutations in the body -->
+        <tr>
+           <th>{gameHeader(rounds)} Digit</th>
+           {#each colorsUsed as c,i}
+           <th style="background-color:{colors[colorPresentationOrder[i]]}">
+                {colorsNames[colorPresentationOrder[i]]} Road
+            </th>
+           {/each}
+           <th>{gameHeader(rounds)} Digit</th>
+        </tr>
+    </thead>
+    <tbody id="tableBody">
+        <!-- For each digit (0-9) enumerate user options
+        then present 'numColors' number of permutations in a random order -->
+        {#each [0,1,2,3,4,5,6,7,8,9] as digit}
+        <tr>
+            <th>{digit} &#62</th>
+            {#each pMatrix as perm,i}
+            <th style="background-color:{colors[colorPresentationOrder[i]]}; 
+                    min-width:{100/(numColors+2)}vw;"
+            >
+                {pMatrix[colorPresentationOrder[i]][digit]}
+            </th>
+            {/each}
+            <th>&#60 {digit}</th>
+        </tr>
+        {/each}
+    </tbody>
+</table>
+<br>
+{#each colorsUsed as c,i}
+<button 
+    class="roads" 
+    style="grid-auto-rows:minmax({30/numColors}vh,auto);
+    background-color:{colors[colorPresentationOrder[i]]}"
+    on:click = {() => pickRoad(c,pMatrix[i])}
+    >   
+    {colorsNames[colorPresentationOrder[i]]} Road
+</button>
+{/each}
+
+
+<style>
+    table {
+        width: 100%;
+    }
+    .roads {
+        display: grid;
+		grid-template-columns: repeat(1, 1fr);
+		min-width: 75vw;
+		border-style: solid;
+        border-color: black;
+        font-size: 1.5em;
+        text-align: center;
+    }
+
+</style>
